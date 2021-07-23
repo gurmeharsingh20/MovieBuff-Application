1d68433369c139332779a6b1313f4bb32426dd80
gurme <mehar-Singh-200>
2021-07-23 14:56:50 +0530

    Movie Buff Application

--- app/src/main/java/com/GurmeharSingh/moviebuff/MainActivity.kt
+++ app/src/main/java/com/GurmeharSingh/moviebuff/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.GurmeharSingh.moviebuff
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import com.android.volley.Request
 import com.android.volley.Response
@@ -14,6 +19,8 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.GurmeharSingh.moviebuff.adapter.MovieItemClicked
 import com.GurmeharSingh.moviebuff.adapter.MovieListAdapter
 import com.GurmeharSingh.moviebuff.model.Movie
+import com.bumptech.glide.Glide
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -21,15 +28,20 @@ class MainActivity : AppCompatActivity(), MovieItemClicked {
 
     private lateinit var mAdapter: MovieListAdapter
     private lateinit var loading_Icon: ProgressBar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var hView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getSupportActionBar()?.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
-        getSupportActionBar()?.setDisplayShowHomeEnabled(true);
-        getSupportActionBar()?.setIcon(R.drawable.ic_baseline_menu_24)
-        getSupportActionBar()?.setTitle("  Popular Movies")
+////        getSupportActionBar()?.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
+////        getSupportActionBar()?.setDisplayShowHomeEnabled(true);
+//        getSupportActionBar()?.setIcon(R.drawable.ic_baseline_menu_24)
+
+        supportActionBar?.title = "Popular Movies"
 
 
         loading_Icon = findViewById(R.id.loading_icon)
@@ -39,12 +51,42 @@ class MainActivity : AppCompatActivity(), MovieItemClicked {
         fetchData()
         mAdapter = MovieListAdapter(this) // 2nd parameter is listener and for that, main activity has implemented interface built in MovieListAdapter.kt file.
         recyclerView.adapter = mAdapter
+
+        //Drawer Code
+
+        navigationView = findViewById(R.id.navigation_view)
+        hView = navigationView.inflateHeaderView(R.layout.nav_header)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        toggle = ActionBarDrawerToggle(this, drawerLayout,R.string.open,R.string.close)
+        toggle.drawerArrowDrawable.setColor(getResources().getColor(R.color.white))
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navigationView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.topRated -> {
+                    val intent = Intent(this,TopRated::class.java)
+                    startActivity(intent)
+                }
+                R.id.nowPlaying -> {
+                    val intent = Intent(this,NowPlaying::class.java)
+                    startActivity(intent)
+                }
+                R.id.upcoming -> {
+                    val intent = Intent(this,Upcoming::class.java)
+                    startActivity(intent)
+                }
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu_item, menu)
-        return super.onCreateOptionsMenu(menu)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun fetchData(){
